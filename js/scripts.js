2686fe833c44eb932dd923f6212c31c23befe978
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-01-26 18:51:13 -0500

    added icons to each section, contact information, project links, generatl information

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,45 @@
 $(document).ready(function() {
+	
 	//handling scroll animation when nav links are clicked
-
-	// $(function() {
 	    $('a.page-scroll').bind('click', function(event) {
 	        var $anchor = $(this);
 	        $('html, body').stop().animate({
-	            scrollTop: ($($anchor.attr('href')).offset().top - 40)
+	            scrollTop: ($($anchor.attr('href')).offset().top - 64)
 	        }, 1500, 'easeInOutExpo');
 	        event.preventDefault();
 	    });
-	// });
 
+	    //handling navbar CSS transitions (To Do: fix animated scroll when name in top left is clicked)
+		$(window).scroll(function () {
+			var elem = $('.fixed');
+			var color = $('.page-scroll');
+			var windowWidth = $(window).width();
+			if ($(this).scrollTop() > 0) {
+				elem.css({"background-color":"black","transition":"2s"});
+				color.css({"color": "white"});
+			}
+			else {
+				elem.css({"background-color":"transparent","transition":".8s"});
+				color.css({"color": "black"});
+			}
+			if (windowWidth <= 768) {
+				color.css({"color": "black", "background-color": "white"})
+			}
+		});
+
+
+		// $(window).resize(function(){
+		// 	var color = $('.page-scroll');
+		// 	var elem = $('.fixed');
+		// 	var windowWidth = $(window).width();
+		// 	if (windowWidth <= 768) {
+		// 		color.css({"color": "black", "background-color": "white"});
+		// 		elem.css({"background-color":"transparent"});
+		// 	}
+		// 	else {
+		// 		color.css({"color": "black"});
+		// 		elem.css({"background-color":"transparent"});
+		// 	}
+		// });
 
 });
